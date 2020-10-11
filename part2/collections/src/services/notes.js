bd770c28025d832c04cd69b6af471b11f02c707b
mrgoudarzi <mr.goudarzi60@gmail.com>
2020-10-10 20:05:51 -0700

    change baseurl to heroku server

--- part2/collections/src/services/notes.js
+++ part2/collections/src/services/notes.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = 'https://secret-earth-35786.herokuapp.com/api/notes'
 
 
 //get all notes from db.json
