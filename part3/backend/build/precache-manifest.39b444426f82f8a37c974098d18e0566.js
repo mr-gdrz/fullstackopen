0ddfc6fae133543b1f8fa535f488776b471c50be
mrgoudarzi <mr.goudarzi60@gmail.com>
2020-10-10 21:33:17 -0700

    add prod to backend

---
+++ part3/backend/build/precache-manifest.39b444426f82f8a37c974098d18e0566.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c23c6facf9f7f97db0765f994ec9f5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2e97d0162f56f9c94cf",
+    "url": "/static/css/main.6e5f4f6f.chunk.css"
+  },
+  {
+    "revision": "5e57e014525dd1bc49af",
+    "url": "/static/js/2.150b86f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.150b86f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2e97d0162f56f9c94cf",
+    "url": "/static/js/main.de5a4d45.chunk.js"
+  },
+  {
+    "revision": "02171bf3522708bd7c3f",
+    "url": "/static/js/runtime-main.4cc291a1.js"
+  }
+]);
